0725991e506460dce7bf705cd70aec23cee03ddd
SoulPredatorNox <petarwiener@gmail.com>
2024-06-10 21:19:49 +0200

    Finished

--- Day1.java
+++
@@ -1,7 +0,0 @@
-import java.io.FileNotFoundException;
-
-public class Day1 {
-    public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("bruh");
-    }
-}
