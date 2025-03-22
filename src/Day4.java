c64e8b7b95406773257a022adc2c9b2ccfae2053
petarwiener <petarwiener@gmail.com>
2025-03-22 09:07:23 +0100

    Day 4 Part 1/2 finished

--- src/Day4.java
+++ src/Day4.java
@@ -8,18 +8,50 @@ For example:
 
 If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043 starts with five zeroes (000001dbbfa...), and it is the lowest such number to do so.
 If your secret key is pqrstuv, the lowest number it combines with to make an MD5 hash starting with five zeroes is 1048970; that is, the MD5 hash of pqrstuv1048970 looks like 000006136ef....
+
+--- Part Two ---
+
+        Now find one that starts with six zeroes.
+
 */
+
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.math.BigInteger;
+import java.security.*;
 import java.util.Scanner;
 
 public class Day4 {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException {
 
-        Scanner in = new Scanner(new FileReader("input/Day3"));
+
+        Scanner in = new Scanner(new FileReader("input/Day4"));
         String s = in.next();
+        int limit=2;
 
+        for (int temp = 1; temp < limit; temp++) {
+            String hash = s+Integer.toString(temp);
+            //String hash= "pqrstuv1048970"; testcase for basic functions
+            System.out.println(hash);
 
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            byte[] messageDigest = md.digest(hash.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashtext = number.toString(16);
+
+            while(hashtext.length() < 32 ){
+                hashtext = "0"+hashtext;
+            }
+
+            System.out.println(hashtext);
+
+            if (hashtext.charAt(0) == '0'&hashtext.charAt(1) == '0'&hashtext.charAt(2) == '0'&hashtext.charAt(3) == '0'&hashtext.charAt(4) == '0'&hashtext.charAt(5) == '0') {
+                System.out.println(hashtext);
+                System.out.println(hash);
+
+            }else limit++;
+        }
     }
 }
