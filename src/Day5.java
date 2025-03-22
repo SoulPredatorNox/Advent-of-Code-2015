d57ac2783a1327acc2ca06540cfecbb3ab6a6c45
petarwiener <petarwiener@gmail.com>
2025-03-22 10:00:46 +0100

    Part 1 Working

--- src/Day5.java
+++ src/Day5.java
@@ -16,6 +16,22 @@ jchzalrnumimnmhp is naughty because it has no double letter.
 haegwjzuvuyypxyu is naughty because it contains the string xy.
 dvszwmarrgswjxmb is naughty because it contains only one vowel.
 How many strings are nice?
+
+--- Part Two ---
+
+Realizing the error of his ways, Santa has switched to a better model of determining whether a string is naughty or nice. None of the old rules apply, as they are all clearly ridiculous.
+
+Now, a nice string is one with all of the following properties:
+
+It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+For example:
+
+qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).
+xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
+ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
+How many strings are nice under these new rules?
 */
 
 
@@ -23,15 +39,50 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
+
+
 public class Day5 {
-    public static void main(String[] args) throws FileNotFoundException {
-
-        Scanner in = new Scanner(new FileReader("input/Day5"));
-        String s = in.next();
 
 
+    public static boolean hasThreeVowels(String s) {
+        int vowels = 0;
+        String temp_eval = s.toLowerCase();
 
+        for (int i = 0; i < temp_eval.length(); i++) {
+            char c = temp_eval.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                vowels++;
+            }
+        }
+        return vowels >= 3;
+    }
+
+    public static boolean hasDoubleLetter(String s) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean containsForbiddenSubstrings(String s) {
+        return s.contains("ab") || s.contains("cd") || s.contains("pq") || s.contains("xy");
     }
 
 
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner in = new Scanner(new FileReader("input/Day5"));
+
+        int nicecheck=0;
+        while (in.hasNext()) {
+            String s = in.next();
+
+            if (hasThreeVowels(s) && hasDoubleLetter(s) && !containsForbiddenSubstrings(s)) {
+                nicecheck++;
+            }
+        }
+        System.out.println(nicecheck);
+    }
 }
